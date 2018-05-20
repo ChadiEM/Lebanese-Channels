2d88f2a05a6298653da1578217be237ad28da832
Chadi El Masri <chadimsr@gmail.com>
2018-05-20 16:16:49 +0200

    Re-added missing NBN

--- lebanese_channels/channel_ids.py
+++ lebanese_channels/channel_ids.py
@@ -5,6 +5,7 @@ from lebanese_channels.services.lbc import LBC
 from lebanese_channels.services.lbcsports import LBCSports
 from lebanese_channels.services.manar import Manar
 from lebanese_channels.services.mtv import MTV
+from lebanese_channels.services.nbn import NBN
 from lebanese_channels.services.noursat import Noursat
 from lebanese_channels.services.otv import OTV
 
@@ -15,6 +16,7 @@ CHANNEL_LIST = [
     OTV(),
     Jadeed(),
     Future(),
+    NBN(),
     Manar(),
     Noursat(),
     LBCSports()
